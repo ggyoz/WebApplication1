96d464fd03edd3633857ab957337cfa0d8936bc7
ggyoz <ggyoz@naver.com>
2025-12-17 16:52:17 +0900

    메뉴관리 수정, 레이아웃 수정 css 파일 노드 수정정

---
+++ ClientApp/js/app.js
@@ -0,0 +1,8 @@
+// Import Bootstrap's JavaScript
+import 'bootstrap';
+
+// Import jQuery
+import $ from 'jquery';
+
+// You can add your own custom JS here
+console.log('app.js loaded');
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6424eb06e34a5b311e186214fd78e9d65571fcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55339d365eee424446f958d235ac816e345b9b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
